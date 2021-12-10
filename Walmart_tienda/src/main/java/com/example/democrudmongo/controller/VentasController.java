4327309c1d1f7b5301cce35e436d5e887505717f
AndersonAlcantar <87408797+AndersonAlcantar@users.noreply.github.com>
2021-12-10 17:48:19 -0500

    Cambios en ventas

--- Walmart_tienda/src/main/java/com/example/democrudmongo/controller/VentasController.java
+++ Walmart_tienda/src/main/java/com/example/democrudmongo/controller/VentasController.java
@@ -3,6 +3,7 @@ package com.example.democrudmongo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +16,7 @@ import com.example.democrudmongo.service.VentasService;
 
 
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE})
 @RequestMapping(path = "/api/v1")
 public class VentasController {
 	
