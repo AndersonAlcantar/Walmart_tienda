4555b5e2a7ef6b57f7afac4395edec936b6e3a94
dannaabril <ginnethabril@gmail.com>
2021-12-10 19:22:47 -0500

    Actualizacionlogin

--- Walmart_tienda/src/main/java/com/example/democrudmongo/controller/LoginController.java
+++ Walmart_tienda/src/main/java/com/example/democrudmongo/controller/LoginController.java
@@ -2,9 +2,11 @@ package com.example.democrudmongo.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.democrudmongo.model.Usuarios;
@@ -14,6 +16,7 @@ import com.example.democrudmongo.service.LoginService;
 
 
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE})
 @RequestMapping(path = "/api/v1")
 public class LoginController {
 	
